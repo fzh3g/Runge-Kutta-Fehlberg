8e37166f4cdbbc7a90bbaf85c90d2554a3d3d240
Solar Qiang <qiang@Solars-N56VZ.local>
2016-04-02 23:25:48 +0800

    Remove Unneccessary parameters; Optimize class structure; Make all arguments private; Change class interface a bit (for good!)

--- include/orbit_ellipse_2d.hpp
+++ include/orbit_ellipse_2d.hpp
@@ -2,7 +2,10 @@
  * orbit_ellipse_2d.hpp
  *
  *  Created on: Fri Mar 18 2016
- *      Author: zhengfaxiang
+ *      Author: zhengfAxiang
+ *
+ *  Last Modified: Sat Apr 2 2016
+ *      Author: solarqiang
  */
 
 #ifndef ORBIT_ELLIPSE_2D_HPP_
@@ -10,26 +13,21 @@
 
 #include <cmath>
 #include <stdexcept>
-#include "orbit_ellipse_2d.hpp"
 
 template<class T>
 class OrbitEllipse2D{
 private:
-    T AngularMomentum(T R, T AngVel);
-    T Velocity(T V_r, T AngMom, T R);
-    T SemiMajorAxis(T Ener, T miu);
-    T Energy(T V, T miu, T R);
-    T Period(T SemiMajorAxis, T miu);
+    T AngularMomentum();
+    T Velocity();
+    T SemiMajorAxis();
     T GetEnergy();
     T GetPeriod();
-    void XY2RTheta(T x, T y, T v_x, T v_y);
-public:
-    OrbitEllipse2D(T x, T y, T v_x, T v_y, T miu);
     T X;                        // x
     T Y;                        // y
     T V_x;                      // V x
     T V_y;                      // V y
     T R;                        // R
+    T Ax;                       // Semimajor Axis
     T V_r;                      // radial velocity
     T Theta;                    // true anomaly
     T AngVel;                   // angular velocity
@@ -38,26 +36,27 @@ public:
     T Ener;                     // energy
     T Peri;                     // Period
     T Miu;                      // miu
+    void XY2RTheta();
+public:
+    OrbitEllipse2D(T x, T y, T v_x, T v_y, T miu);
+    T Energy(){return Ener;}
+    T Period(){return Peri;}
 };
 
+// 
+// template<class T>
+// T OrbitEllipse2D<T>::Velocity() {
+//     return sqrt(V_r * V_r + AngMom * AngMom / (R * R));
+// }
+
 template<class T>
-T OrbitEllipse2D<T>::AngularMomentum(T R, T AngVel) {
-    return AngVel * R * R;
+T OrbitEllipse2D<T>::SemiMajorAxis() {
+    return -Miu / (Ener * 2.0);
 }
 
 template<class T>
-T OrbitEllipse2D<T>::Velocity(T V_r, T AngMom, T R) {
-    return sqrt(V_r * V_r + AngMom * AngMom / (R * R));
-}
-
-template<class T>
-T OrbitEllipse2D<T>::SemiMajorAxis(T Ener, T miu) {
-    return -miu / (Ener * 2.0);
-}
-
-template<class T>
-T OrbitEllipse2D<T>::Energy(T V, T miu, T R) {
-    T energy = V * V / 2.0 - miu / R;
+T OrbitEllipse2D<T>::GetEnergy() {
+    T energy = V * V / 2.0 - Miu / R;
     if (energy > 0) {
         throw std::invalid_argument("Energy greater than zero!");
     }
@@ -65,40 +64,27 @@ T OrbitEllipse2D<T>::Energy(T V, T miu, T R) {
 }
 
 template<class T>
-T OrbitEllipse2D<T>::Period(T SemiMajorAxis, T miu) {
-    return 2 * atan(1) * 4 * sqrt(pow(SemiMajorAxis, 3.0) / miu);
-}
-
-template<class T>
-T OrbitEllipse2D<T>::GetEnergy() {
-    T velo = Velocity(V_r, AngMom, R);
-    return Energy(velo, Miu, R);
-}
-
-template<class T>
 T OrbitEllipse2D<T>::GetPeriod() {
-    T ener = GetEnergy();
-    T a = SemiMajorAxis(ener, Miu);
-    return Period(a, Miu);
+    return 2 * atan(1) * 4 * sqrt(pow(Ax, 3.0) / Miu);
 }
 
 template<class T>
-void OrbitEllipse2D<T>::XY2RTheta(T x, T y, T v_x, T v_y) {
-    R = sqrt(x * x + y * y);
-    V = sqrt(v_x * v_x + v_y * v_y);
-    Theta = atan2(y, x);
-    T alpha = acos((x * v_x + y * v_y) / (R * V));
-    V_r = V * cos(alpha);
-    AngVel = V * sin(alpha) / R;
-}
+OrbitEllipse2D<T>::OrbitEllipse2D(T x, T y, T V_x, T V_y, T miu) {
+    X = x, Y = y, V_x = V_x, V_y = V_y;
+    Miu     = miu;
 
-template<class T>
-OrbitEllipse2D<T>::OrbitEllipse2D(T x, T y, T v_x, T v_y, T miu) {
-    XY2RTheta(x, y, v_x, v_y);
-    AngMom = AngularMomentum(R, AngVel);
-    Miu = miu;
-    Ener = GetEnergy();
-    Peri = GetPeriod();
+    R       = sqrt(X * X + Y * Y);
+    V       = sqrt(V_x * V_x + V_y * V_y);
+    Theta   = atan2(Y, X);
+    T alpha = acos((X * V_x + Y * V_y) / (R * V));
+    V_r     = V * cos(alpha);
+
+    AngVel  = V * sin(alpha) / R;
+    AngMom  = V * sin(alpha) * R;
+
+    Ener    = GetEnergy();
+    Ax      = SemiMajorAxis();
+    Peri    = GetPeriod();
 }
 
 #endif  /* ORBIT_ELLIPSE_2D_HPP_ */
