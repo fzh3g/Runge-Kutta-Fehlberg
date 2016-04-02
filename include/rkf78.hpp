aa4bd2a090a4bdf16b936eeb7adfcbe64be50b90
Solar Qiang <qiang@Solars-N56VZ.local>
2016-04-02 22:33:13 +0800

    Remove unnecessary parameters in member functions

--- include/rkf78.hpp
+++ include/rkf78.hpp
@@ -27,19 +27,30 @@ private:
     T t;                        // time
     T K[13][dim];               // runge-kutta parameters
     T R[dim];                   // error
+    T y[dim];                   // position and velocity
     T z[13][dim];               // make calculation of Ks easier
-    void GetZ(int l, T y[dim]);
-    void RungeKuttaParams78(T t, T h, T y[dim]);
-    void GetY(T y[dim]);
-    T hnew(T h, T hmax, T hmin, T q);
+    void GetZ(int l);
+    void RungeKuttaParams78();
+    void GetY();
+    T hnew(T hmax, T hmin, T q);
+    void SetY(T iny[dim]);
 public:
     long step;                  // step
     T (*f[dim])(T t, T y[dim]); // functions to solve
-    void rkf78(T hmax, T hmin, T *hnow, T *time, T ynow[dim], T TOL);
+    void rkf78(T hmax, T hmin,  T TOL);
     void solve(T hmax, T hmin, T y0[dim], T TOL, T begin, T end,
                const char *filename);
 };
 
+
+template<class T, int dim>
+void RKF78<T, dim>::SetY(T iny[dim])
+{
+    for(int i=0;i<dim;++i)
+    {
+        y[i]=iny[i];
+    }
+}
 template<class T, int dim>
 const T RKF78<T, dim>::a[13] = {
     0.0, 2.0/27.0, 1.0/9.0, 1.0/6.0, 5.0/12.0, 0.5,
@@ -73,7 +84,7 @@ const T RKF78<T, dim>::b[13][12] = {
 };
 
 template<class T, int dim>
-void RKF78<T, dim>::GetZ(int l, T y[dim]) {
+void RKF78<T, dim>::GetZ(int l) {
     T tmp[dim];
     for (int i=0; i < dim; i++) {
         tmp[i] = 0;
@@ -91,10 +102,10 @@ void RKF78<T, dim>::GetZ(int l, T y[dim]) {
 }
 
 template<class T, int dim>
-void RKF78<T, dim>::RungeKuttaParams78(T t, T h, T y[dim]) {
+void RKF78<T, dim>::RungeKuttaParams78() {
     // get runge-kutta parameters
     for (int j=0; j < 13; j++) {
-        GetZ(j, y);
+        GetZ(j);
         for (int i=0; i < dim; i++) {
             K[j][i] = h * (*f[i])(t + h * a[j], z[j]);
         }
@@ -102,7 +113,7 @@ void RKF78<T, dim>::RungeKuttaParams78(T t, T h, T y[dim]) {
 }
 
 template<class T, int dim>
-void RKF78<T, dim>::GetY(T y[dim]) {
+void RKF78<T, dim>::GetY() {
     // get y using runge-kutta parameters
     for (int i=0; i < dim; i++) {
         y[i] += K[5][i] * 34.0 / 105.0 +
@@ -113,7 +124,7 @@ void RKF78<T, dim>::GetY(T y[dim]) {
 }
 
 template<class T, int dim>
-T RKF78<T, dim>::hnew(T h, T hmax, T hmin, T q) {
+T RKF78<T, dim>::hnew(T hmax, T hmin, T q) {
     // get new h
     if (q <= 0.1) {
         h = h * 0.1;
@@ -132,24 +143,24 @@ T RKF78<T, dim>::hnew(T h, T hmax, T hmin, T q) {
 }
 
 template<class T, int dim>
-void RKF78<T, dim>::rkf78(T hmax, T hmin, T *hnow, T *time,
-                            T ynow[dim], T TOL) {
+void RKF78<T, dim>::rkf78(T hmax, T hmin, 
+                             T TOL) {
     // function to apply the runge-kutta-fehlberg method for one step
-    for (;;) {
-        RungeKuttaParams78(*time, *hnow, ynow);  // get Ks
+     while(true) {
+        RungeKuttaParams78();  // get Ks
         for (int i=0; i < dim; i++) {            // finding errors
             R[i] = fabs(K[0][i] + K[10][i] - K[11][i] - K[12][i])\
-                * (*hnow) * 41.0 / 810.0;
+                * (h) * 41.0 / 810.0;
         }
         T MaxErr = *max_element(R, R + dim); // maximium value of array R
         T q=pow(TOL / (MaxErr * 2.0), 1.0 / 7.0);
         if (MaxErr < TOL) {
-            *time += *hnow;
-            GetY(ynow);         // get Ys
+            t += h;
+            GetY();         // get Ys
             break;
         }
-        *hnow = hnew(*hnow, hmax, hmin, q); // new h
-        if (*hnow < 0){                     // error handling
+        h = hnew(hmax, hmin, q); // new h
+        if (h < 0){                     // error handling
             throw invalid_argument("Minimum h exceeded!");
         }
     }
@@ -165,6 +176,7 @@ void RKF78<T, dim>::solve(T hmax, T hmin, T y0[dim], T TOL, T begin, T end,
     t = begin;             // begin of t
     h = hmax;              // begin of h
     step = 0;              // step
+    SetY(y0);
     // output header
     cout<<setw(28)<<"t"<<setw(28)<<"h";
     outfile<<setw(28)<<"t"<<setw(28)<<"h";
@@ -179,7 +191,7 @@ void RKF78<T, dim>::solve(T hmax, T hmin, T y0[dim], T TOL, T begin, T end,
     cout<<endl;
     outfile<<endl;
     for (;t < end;) {
-        rkf78(hmax, hmin, &h, &t, y0, TOL); // calculate one step
+        rkf78(hmax, hmin,  TOL); // calculate one step
         step++;                               // step plus one
         // output result
         cout<<setiosflags(ios::scientific)
@@ -190,9 +202,9 @@ void RKF78<T, dim>::solve(T hmax, T hmin, T y0[dim], T TOL, T begin, T end,
                <<setprecision(18)<<setw(28)<<h;
         for (int i=0; i < dim; i++) {
             cout<<setiosflags(ios::scientific)<<setprecision(18)
-                <<setw(28)<<y0[i];
+                <<setw(28)<<y[i];
             outfile<<setiosflags(ios::scientific)<<setprecision(18)
-                   <<setw(28)<<y0[i];
+                   <<setw(28)<<y[i];
         }
         cout<<endl;
         outfile<<endl;
