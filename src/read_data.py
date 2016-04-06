c963aaa7b17a80ea60dbc88c53ef5ef69db956e9
Faxiang Zheng <fxzheng0906@outlook.com>
2016-04-06 18:59:33 +0800

    read data

---
+++ src/read_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def read_cols(filename, cols, header=0):
+    """Read two columns of data in a file"""
+    f = open(filename, 'r')
+    if header > 0:
+        for i in range(header):
+            f.readline()
+    numberofcol = len(cols)
+    data = [[] for i in range(numberofcol)]
+    for line in f:
+        line = line.strip()
+        columns = line.split()
+        for i in range(numberofcol):
+            data[i].append(np.longdouble(columns[cols[i]]))
+    f.close()
+    return data
+
