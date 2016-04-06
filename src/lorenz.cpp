929a896b49e5a4092f0aed95b0aa61e21ebbc890
Faxiang Zheng <fxzheng0906@outlook.com>
2016-04-06 19:00:40 +0800

    lorenz attractor

---
+++ src/lorenz.cpp
@@ -0,0 +1,47 @@
+/*
+ * lorenz.cpp
+ *
+ *  Created on: Sat April 3 2016
+ *      Author: zhengfaxiang
+ */
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "../include/rkf78.hpp"
+using namespace std;
+
+
+template<class T>
+T lorenzf0(T t, T y[3]) {
+    return -10 * y[0] + 10 * y[1];
+}
+
+template<class T>
+T lorenzf1(T t, T y[3]) {
+    return 28 * y[0] - y[1] - y[0] * y[2];
+}
+
+template<class T>
+T lorenzf2(T t, T y[3]) {
+    return -8/3 * y[2] + y[0] * y[1];
+}
+
+int main(int argc, char *argv[]) {
+    const char *datafile = "lorenz.dat";    
+    RKF78<long double, 3> RKF;
+    RKF.f[0] = &lorenzf0<long double>;
+    RKF.f[1] = &lorenzf1<long double>;
+    RKF.f[2] = &lorenzf2<long double>;
+    long double rkf[3] = {1.0, 1.0, 0.0};
+    try {
+        RKF.solve(0.1, 1e-8, rkf, 1e-12, 0.0, 50, datafile);
+    } catch (invalid_argument& e) {
+        cerr << e.what() << endl;
+        return -1;
+    }
+    cout<<"Steps: "<<RKF.step<<endl;    
+    return 0;
+}
+
---
+++ src/plot_data_3d.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import read_data
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+datafile = "../data/lorenz.dat"
+datacols = [2, 3, 4]
+data = read_data.read_cols(datafile, datacols, header=1)
+
+ax.plot(data[0], data[1], data[2], marker='.', markersize=2.0,
+        linestyle='None', color='r')
+ax.set_title('Lorenz Attractor', fontsize=16)
+ax.dist = 12
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+# bbox = (-2.0, 2.0)
+# xmin, xmax, ymin, ymax, zmin, zmax = bbox*3
+# ax.set_zlim3d(zmin, zmax)
+# ax.set_xlim3d(xmin, xmax)
+# ax.set_ylim3d(ymin, ymax)
+
+plt.savefig('../img/lorenz.png')
+plt.show()
