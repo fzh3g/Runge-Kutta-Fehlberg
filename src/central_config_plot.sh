3a8799f081e1888c5b8069b455e5cc1c55c7f697
Faxiang Zheng <fxzheng0906@outlook.com>
2016-05-22 02:16:02 +0800

    simple_plot tex keyword

--- src/central_config_plot.sh
+++ src/central_config_plot.sh
@@ -3,21 +3,21 @@
 echo "Plotting phi-t figure..."
 
 ../src/simple_plot.py ../data/central_config.dat --columns 0 2 \
-                      --labels '$t$' '$\phi{}$' \
+                      --labels 't' '\phi{}' --tex \
                       --figname 'central_config_phi' \
                       --xlim 0 20 --ylim 0 12 --figtype 'png'
 
 echo "Plotting phidot-t figure..."
 
 ../src/simple_plot.py ../data/central_config.dat --columns 0 3 \
-                      --labels '$t$' '$d\phi{}/dt$' \
+                      --labels 't' 'd\phi{}/dt' --tex \
                       --figname 'central_config_phidot' \
                       --xlim 0 20 --ylim -4 1 --figtype 'png'
 
 echo "Plotting phidot-phi figure..."
 
 ../src/simple_plot.py ../data/central_config.dat --columns 2 3 \
-                      --labels '$\phi{}$' '$d\phi{}/dt$' \
+                      --labels '\phi{}' 'd\phi{}/dt' --tex \
                       --figname 'central_config_phiphase' \
                       --xlim 0 8 --ylim -4 1 --figtype 'png'
 
